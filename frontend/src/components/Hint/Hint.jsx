59dca99a20331604dc2d92c99da9989f1ffa7658
Pablo <pablo.silva61@fatec.sp.gov.br>
2025-11-23 03:47:48 -0300

    fix(#113): destinando arquivo css pra pasta correta

--- frontend/src/components/Hint/Hint.jsx
+++ frontend/src/components/Hint/Hint.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import "./Hint.css";
+import "../../style/Hint.css";
 const Hint = ({ text, position = "top", size = "md" }) => {
   const [isVisible, setIsVisible] = React.useState(false);
 
